2f8a4eb71e34a53b4dffd905569736bf58eb1e05
klord <tanhuan.klord@gmail.com>
2021-04-27 11:09:41 +0700

    Updates

---
+++ precache-manifest.a24d56a2e617c933781367a3c161bce1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba87425a7d23fd7e113a",
+    "url": "/static/css/main.cfb7b71f.chunk.css"
+  },
+  {
+    "revision": "ba87425a7d23fd7e113a",
+    "url": "/static/js/main.ba87425a.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "32abe8d3867fd1bc13cb",
+    "url": "/static/css/2.ddfdb3f5.chunk.css"
+  },
+  {
+    "revision": "32abe8d3867fd1bc13cb",
+    "url": "/static/js/2.32abe8d3.chunk.js"
+  },
+  {
+    "revision": "4e59f7c5f6275ec64349f95e8e2bdf29",
+    "url": "/static/media/18.4e59f7c5.jpg"
+  },
+  {
+    "revision": "b49d6c3f88dfb3d7be9f474335ec8c4b",
+    "url": "/index.html"
+  }
+];
